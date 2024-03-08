78349be5bc9980fecc0911e9b0a85d2683c657de
Reinel <reinel@panita.ai>
2024-03-07 22:14:28 -0400

    update node version

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,8 +2,9 @@
 
 # Install node.js
 sudo apt-get install python-software-properties -y
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get update
-sudo apt-get install nodejs -y
+sudo apt install nodejs -y
 sudo apt install npm -y
 sudo apt-get install iptables -y
 
