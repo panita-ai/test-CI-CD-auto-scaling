aa42f43746da6cea1378b6105e1cbe3f01cef268
Reinel <reinel@panita.ai>
2024-03-07 23:04:09 -0400

    refactor app.js

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const fs = require('node:fs/promises');
+const fs = require("node:fs/promises");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+const filePath = path.join(__dirname, "data.json");
 
 app.use(bodyParser.json());
 
-app.get('/', async function (req, res) {
+app.get("/", async function (req, res) {
   let dummyData;
   for (let i = 0; i < 5000; i++) {
-    dummyData = await fs.readFile('data.json');
+    dummyData = await fs.readFile(filePath);
   }
   const json = JSON.parse(dummyData);
-  res.json({ message: 'Success!', data: json });
+  res.json({ message: "Success!", data: json });
 });
 
-app.post('/data', async function (req, res) {
+app.post("/data", async function (req, res) {
   const data = req.body;
 
-  res.status(201).json({ message: 'Received dummy data.', data });
+  res.status(201).json({ message: "Received dummy data.", data });
 });
 
 app.listen(3000);
