ded5f3e23bab2d90944311769b48f079f973bdd0
Reinel <reinel@panita.ai>
2024-03-07 22:28:45 -0400

    remove sudo

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,7 @@
 
 # Install node.js
 sudo apt-get install python-software-properties -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | sudo bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
