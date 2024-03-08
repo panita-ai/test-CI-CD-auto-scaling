70cb9975419d5b87fd6519dce6d516e20cf7331d
Reinel <reinel@panita.ai>
2024-03-07 22:36:54 -0400

    nvm use

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,6 @@
 sudo rm -rf /home/ubuntu/nodejs
 mkdir /home/ubuntu/nodejs
 cd /home/ubuntu/nodejs
-
+nvm install 18.16.0
+nvm use 18.16.0
 npm install
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,7 +6,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-nvm install --lts
+nvm install 18.16.0
+nvm use 18.16.0
 sudo apt-get install iptables -y
 
 # Clean working folder
