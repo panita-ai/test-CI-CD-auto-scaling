81dda37693e2f59f6b1151298fd853c67155dd24
Reinel <reinel@panita.ai>
2024-03-07 22:18:32 -0400

    install node with nvm

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,18 +2,12 @@
 
 # Install node.js
 sudo apt-get install python-software-properties -y
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get update
-sudo apt install nodejs -y
-sudo apt install npm -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install --lts
 sudo apt-get install iptables -y
-
-# Install nodemon
-# sudo npm install nodemon -g
-
-# Install forever module 
-# https://www.npmjs.com/package/forever
-sudo npm install forever -g
 
 # Clean working folder
 # sudo find /home/ubuntu/test -type f -delete
